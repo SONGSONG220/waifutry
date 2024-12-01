69de73e6160fe72b726631030d31d2a37101e77e
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-12-01 12:17:39 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "7453770651"
-    sudo_users = "7453770651", "7499006737", "7096345827"
-    GROUP_ID = -1001975521991
-    TOKEN = "7378765314:AAH1tmo0EMQ4-g0lFVJUxTr0Q7Lg8MXJw8s"
-    mongo_url = "mongodb+srv://vikas:vikas@vikas.yfezexk.mongodb.net/?retryWrites=true&w=majority"
+    OWNER_ID = "7078181502"
+    sudo_users = "7378476666", "7180469677", "7078181502"
+    GROUP_ID = -1002009280180
+    TOKEN = "7698227314:AAGm91L70r1mYcnYc5oGcz_R4AHuNl899aw"
+    mongo_url = "mongodb+srv://I-LOVE-PDF-BOT:I-LOVE-PDF-BOT@cluster0.c51o3a9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/dc0aa314d28a67af0ee83.jpg", "https://telegra.ph/file/e3bdc6e1f14191e058ea7.jpg", "https://telegra.ph/file/dc0aa314d28a67af0ee83.jpg"]
-    SUPPORT_CHAT = "-1002154364668"
-    UPDATE_CHAT = "LUFFY BOTS"
-    BOT_USERNAME = "capture_Waifu_Bot"
-    CHARA_CHANNEL_ID = "-1002154364668"
+    SUPPORT_CHAT = "-1002289810575"
+    UPDATE_CHAT = "PiratesBotRepo"
+    BOT_USERNAME = "husbanduCollector_Bot"
+    CHARA_CHANNEL_ID = "-1002412957777"
     api_id = 22792918
     api_hash = "ff10095d2bb96d43d6eb7a7d9fc85f81"
     
