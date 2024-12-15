53da4949bdb44b56da19e2fd1b8c4fa4b11d6931
Song <satypaldhakad0@gmail.com>
2024-12-15 20:22:31 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "7078181502"
-    sudo_users = "7378476666", "7180469677", "7078181502"
-    GROUP_ID = -1002009280180
+    OWNER_ID = "7526369190"
+    sudo_users = "5268691896", "7526369190", "5884969921"
+    GROUP_ID = -1002468653401
     TOKEN = "7698227314:AAGm91L70r1mYcnYc5oGcz_R4AHuNl899aw"
     mongo_url = "mongodb+srv://I-LOVE-PDF-BOT:I-LOVE-PDF-BOT@cluster0.c51o3a9.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/dc0aa314d28a67af0ee83.jpg", "https://telegra.ph/file/e3bdc6e1f14191e058ea7.jpg", "https://telegra.ph/file/dc0aa314d28a67af0ee83.jpg"]
-    SUPPORT_CHAT = "-1002289810575"
-    UPDATE_CHAT = "PiratesBotRepo"
+    PHOTO_URL = ["https://files.catbox.moe/hsyqp7.jpg", "https://files.catbox.moe/hsyqp7.jpg", "https://files.catbox.moe/hsyqp7.jpg"]
+    SUPPORT_CHAT = "-1002466009612"
+    UPDATE_CHAT = "niyoto_supoort"
     BOT_USERNAME = "husbanduCollector_Bot"
-    CHARA_CHANNEL_ID = "-1002412957777"
+    CHARA_CHANNEL_ID = "-1002183028236"
     api_id = 22792918
     api_hash = "ff10095d2bb96d43d6eb7a7d9fc85f81"
     
